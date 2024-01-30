ef5c4afdf7c3e754fb5868cc4671cce355becdc4
Shailendra Rai <shailendrarai153@gmail.com>
2024-01-30 15:35:06 +0530

    Add files via upload

---
+++ Lecture-1/first.js
@@ -0,0 +1,32 @@
+// const pi = 3.14;
+// console.log(pi);
+
+// let  name  =" shailendr    "
+
+// console.log(name);
+
+// console.log(name.length);
+
+// let tr = name.trim();
+
+// // console.log( ` trim method used ${tr.length}`);
+
+// console.log(name.trim().length);
+
+// console.log(name.slice(0,7));
+
+// String concation
+
+//  let string  = "shai";
+// wa let string_1 = "lend"
+//  let newString = string +string_1;
+//  console.log(newString);
+
+
+//  ternary operator 
+
+  let age =19;
+
+  let drink = age>=18 ? "wine" : "water";
+
+  console.log(drink);
